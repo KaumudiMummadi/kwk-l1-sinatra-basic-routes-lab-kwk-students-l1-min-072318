04fe027c6de5a3ee16aac2cb8d1e965360c3574e
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-31 16:09:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
 end
+  get '/name' do 
+    "My name is Kaumudi"
+  end 
+  get '/hometown' do 
+    "My hometown is Rosemount"
+  end 
+  get '/song' do 
+    "My favorite song is City of Stars"
+  end 
+end 
